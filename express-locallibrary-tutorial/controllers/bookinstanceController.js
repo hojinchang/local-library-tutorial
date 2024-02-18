f6c771711bdce5036e28d6318a2639bbbd8ac515
hojinchang <72773536+hojinchang@users.noreply.github.com>
2024-02-17 20:05:07 -0800

    Add book instance updating code

--- express-locallibrary-tutorial/controllers/bookinstanceController.js
+++ express-locallibrary-tutorial/controllers/bookinstanceController.js
@@ -3,6 +3,7 @@ const Book = require("../models/book");
 
 const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
+const book = require("../models/book");
 
 
 // Display list of all BookInstances.
@@ -135,6 +136,44 @@ exports.bookinstance_update_get = asyncHandler(async (req, res, next) => {
 });
 
 // Handle bookinstance update on POST.
-exports.bookinstance_update_post = asyncHandler(async (req, res, next) => {
-  
-});
+exports.bookinstance_update_post = [
+  body("book", "Book must be specified")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("imprint", "Imprint must be specified")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("status").escape(),
+  body("due_back", "Invalid date")
+    .optional({ values: "falsy" })
+    .isISO8601()
+    .toDate(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const bookInstance = new BookInstance({
+      book: req.body.book,
+      imprint: req.body.imprint,
+      status: req.body.status,
+      due_back: req.body.due_back,
+      _id: req.params.id
+    });
+
+    if (!errors.isEmpty()) {
+      res.render("bookinstance_form", {
+        title: "Update Book Instance",
+        bookinstance: bookInstance,
+        selected_book: bookInstance.book._id,
+        book_list: allBooks,
+        errors: errors.array()
+      });
+      return;
+    } else {
+      await BookInstance.findByIdAndUpdate(req.params.id, bookInstance);
+      res.redirect(BookInstance.url);
+    }
+  }),
+];
